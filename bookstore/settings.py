ff0dcec5fe5304b2bf6743d9746ab922418ddf86
George <georgebrooks.camps@gmail.com>
2025-10-19 23:48:47 -0300

    Adicionando autenticacao em order e retornando mensagem

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside th     e project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -109,8 +109,13 @@ INTERNAL_IPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ]
 }
 
 # Internationalization
--- order/views.py
+++ order/views.py
@@ -1,13 +1,9 @@
-from django.shortcuts import render
-from rest_framework.permissions import IsAuthenticated
-
-from order import viewsets
+from rest_framework import viewsets
 from order.models.order import Order
 from order.serializers.order_serializer import OrderSerializer
-
-# Create your views here.
+from .permissions import IsAuthenticatedCustom
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedCustom]
