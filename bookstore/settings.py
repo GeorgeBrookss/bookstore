1054a42e3aa4c1429e23a2d0e866168e36e16798
George <georgebrooks.camps@gmail.com>
2025-10-20 15:02:02 -0300

    Corrige configuração de STATIC_ROOT para deploy

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -97,6 +97,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/GMarceloB/bookstore/staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
